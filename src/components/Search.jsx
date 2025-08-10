93bd8556a04815ec8bbfc1dc62961f21dea51ff8
Nika Chitanava <chitanava@gmail.com>
2025-08-10 12:11:24 +0400

    feat: add shortcut to focus search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,24 @@
+import {useEffect, useRef} from "react";
+
 export default function Search() {
+    const inputRef = useRef(null);
+
+    useEffect(() => {
+        function callback(e) {
+            if ((e.metaKey || e.ctrlKey) && e.key.toLowerCase() === 'k') {
+                e.preventDefault();
+
+                inputRef.current.focus();
+            }
+        }
+
+        document.documentElement.addEventListener('keydown', callback)
+
+        return () => {
+            document.documentElement.removeEventListener('keydown', callback)
+        }
+    }, [])
+
     return (
         <label className="input w-full">
             <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
@@ -13,7 +33,7 @@ export default function Search() {
                     <path d="m21 21-4.3-4.3"></path>
                 </g>
             </svg>
-            <input type="search" className="grow" placeholder="Search"/>
+            <input ref={inputRef} type="search" className="grow" placeholder="Search"/>
             <kbd className="kbd kbd-sm">⌘ K</kbd>
         </label>
     );
