facaa2125afd77339cf40b376fed04e6dcabc882
Nika Chitanava <chitanava@gmail.com>
2025-08-10 20:52:57 +0400

    refactor(poster): add loaded state

--- src/components/Poster.jsx
+++ src/components/Poster.jsx
@@ -1,27 +1,38 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
-export default function Poster({poster, title}) {
+export default function Poster({poster, title, className = 'rounded-2xl w-24 h-36', iconSize = 6}) {
     const [error, setError] = useState(false);
+    const [loaded, setLoaded] = useState(false);
 
-    return !poster || error
-        ? (
-            <div className='rounded-2xl w-24 h-36 bg-base-content/20 flex items-center justify-center'>
+    useEffect(() => {
+        if (!poster) return;
+
+        const img = new Image();
+
+        img.src = poster;
+        img.onload = () => setLoaded(true);
+        img.onerror = () => setError(true);
+    }, [poster]);
+
+    if (!poster || !loaded || error) {
+        return (
+            <div className={`bg-base-content/20 flex items-center justify-center ${className}`}>
                 <span className='text-base-content/50'>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                          strokeWidth={1.5}
-                         stroke="currentColor" className="size-6">
+                         stroke="currentColor" className={`size-${iconSize}`}>
                         <path strokeLinecap="round" strokeLinejoin="round"
                               d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"/>
                     </svg>
                 </span>
             </div>
-        )
-        : (
-            <img
-                className='rounded-2xl w-24 h-36 object-cover object-center'
-                src={poster}
-                alt={`Poster of ${title}`}
-                onError={() => setError(true)}
-            />
         );
+    }
+
+    return (
+        <img
+            className={`object-cover object-center ${className}`}
+            src={poster}
+            alt={`Poster of ${title}`}/>
+    );
 }
